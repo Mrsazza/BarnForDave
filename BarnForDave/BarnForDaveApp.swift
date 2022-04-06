e9f07f4376def3e72fe366da0b8fb649cfd72a07
Mrsazza <shahriarhossain147@gmail.com>
2022-04-06 23:38:25 +0600

    Initial Commit

---
+++ BarnForDave/BarnForDaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarnForDaveApp.swift
+//  BarnForDave
+//
+//  Created by Sazza on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BarnForDaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
